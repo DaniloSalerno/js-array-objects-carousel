c725140d40f82d3ec7314d3cc9da0bc73a6e82a2
DaniloSalerno <salerno.danilo@hotmail.it>
2023-09-07 16:53:13 +0200

    refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,210 +26,132 @@ const slides = [
     {
         img: './assets/img/03.webp',
     },
-    
+
     {
         img: './assets/img/04.webp',
     },
-    
+
     {
         img: './assets/img/05.webp',
-    }    
-  ]
-  
-  let activeSlide = 0;
-  
-  // select the dom elements
-  const sliderImagesEl = document.querySelector('.slider .images')
-  const prevEl = document.querySelector('.prev')
-  const nextEl = document.querySelector('.next')
+    }
+]
 
-  const pausePlayEl = document.querySelector('.pause_play')
-  
-  
-  //console.log(sliderImagesEl);
-  
-  /* Print all images into the dom */
-  // loop over the slides 
-slides.forEach((element,index) => {
+let activeSlide = 0;
+
+// select the dom elements
+const sliderImagesEl = document.querySelector('.slider .images');
+
+const prevEl = document.querySelector('.prev');
+
+const nextEl = document.querySelector('.next');
+
+const pausePlayEl = document.querySelector('.pause_play');
+
+const slidesImages = document.querySelectorAll('.slider .images > img');
+
+const thumbsElement = document.querySelector('.thumbnails');
+
+/* Print all images into the dom */
+// loop over the slides 
+slides.forEach((element, index) => {
     const slidePath = element;
-    console.log(slidePath);    
+    console.log(slidePath);
 
     // for each slide we create the markup
-    const slideMarkup = `<img class="${activeSlide === index ? 'active' : '' }" src="${slidePath.img}" alt="">`
-    //console.log(slideMarkup);
-  
+    const slideMarkup = `<img class="${activeSlide === index ? 'active' : ''}" src="${slidePath.img}" alt="">`
+
     sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
 });
-  
-  
-  /* 
-  
-  if(condition) {
-    // code to run
-  } else {
-    // code to run
-  }
-  
-  // terary operator
-  
-  condition ? 'code to run' : 'code to run'
-  
-  */
-  
-  
-  /* 
-  
-  MILESTONE 3
-  Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
-  
-  */
-  
-  const slidesImages = document.querySelectorAll('.slider .images > img')
-  console.log(slidesImages);
-  
-  
-  
-  /* 
-  BONUS 1:
-  Aggiungere il ciclo infinito del carosello. Ovvero se è attiva la prima immagine e l'utente clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
-  
-  */
-  
-  /* 
-  
-  BONUS 2:
-  Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, 
-  come nello screenshot proposto. Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato. 
-  Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
-  
-  */
-  
-  
-  const thumbsElement = document.querySelector('.thumbnails')
-/*   
-  for (let i = 0; i < slides.length; i++) {
-    const thumbPath = slides[i];
-    const thumbMarkup = `<img class="${activeSlide === i ? 'active' : ''}" src="${thumbPath}" alt="">`
-    //console.log(thumbMarkup);
-  
-    thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
-    
-  } */
 
-  slides.forEach((element,index) => {
+slides.forEach((element, index) => {
     const thumbPath = element;
-    console.log(thumbPath);    
+    console.log(thumbPath);
 
     // for each slide we create the markup
-    const thumbMarkup = `<img class="thumb ${activeSlide === index ? 'active' : '' }" src="${thumbPath.img}" alt="">`
-    //console.log(slideMarkup);
-  
+    const thumbMarkup = `<img class="thumb ${activeSlide === index ? 'active' : ''}" src="${thumbPath.img}" alt="">`
+
     thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
 });
-  
-    let autoPrevPlay;
-    let autoNextPlay;
+
+let autoPrevPlay;
+let autoNextPlay;
 
 
-  
-  // intercept click on the next icon 
-  nextEl.addEventListener('click', () => {
+
+// intercept click on the next icon 
+nextEl.addEventListener('click', () => {
     clearInterval(autoNextPlay)
+    clearInterval(autoPrevPlay)
+    autoNextPlay = setInterval(nextPlay, 1000);
+  
 
-      autoNextPlay = setInterval(nextPlay, 1000);
-      clearInterval(autoPrevPlay)
+});
 
-  });
 
-  function nextPlay() {  
-      
 
-      console.log('cliccato su next');
+function nextPlay() {
+
     // select the current slide
     const currentSlide = slidesImages[activeSlide]
     console.log(currentSlide);
     // remove the active class from the current slide
     currentSlide.classList.remove('active')
-  
+
     // select the active thumb
     const currentThumb = document.querySelector('.thumbnails > img.active')
     console.log(currentThumb);
     // remove the active class from the active thumb
     currentThumb.classList.remove('active')
-  
-    
-    // activeSlide = 4
-  
+
     if (activeSlide === slidesImages.length - 1) {
-      activeSlide = 0
-      // activeSlide = 5
+        activeSlide = 0
+        // activeSlide = 5
     } else {
-      // increment the activeSlide of 1
-      activeSlide++
+        // increment the activeSlide of 1
+        activeSlide++
     }
-  
-  
+
     // select the next slide
     const nextSlide = slidesImages[activeSlide]
     console.log(nextSlide);
     // add the active class to the next slide
     nextSlide.classList.add('active')
-  
-  
-    /* TODO */
-  
-  
+
     // select the next thumb
     const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
     console.log(nextThumb);
     // add to the next thumb the active class
     nextThumb.classList.add('active')
-  
-  
-  }
-  
-  // intercept click on the prev icon
-
-  
-  // activeSlide = 0
-  prevEl.addEventListener('click', () => {
-    clearInterval(autoPrevPlay)
-
-      clearInterval(autoNextPlay)
-      autoPrevPlay = setInterval(prevPlay, 1000);
-
-  })
 
 
-  function prevPlay() {
-    
-  
-    console.log('cliccato su prev');
-  
-    
-  
+}
+
+
+
+// intercept click on the prev icon
+prevEl.addEventListener('click', () => {
+
+    clearInterval(autoPrevPlay);
+    clearInterval(autoNextPlay);
+    autoPrevPlay = setInterval(prevPlay, 1000);
+
+})
+
+
+function prevPlay() {
+
     // select the current slide
-    const currentSlide = slidesImages[activeSlide]
+    const currentSlide = slidesImages[activeSlide];
     console.log(currentSlide);
     // remove the active class from the current slide
-    currentSlide.classList.remove('active')
+    currentSlide.classList.remove('active');
 
+    // select the active thumb
+    const currentThumb = document.querySelector('.thumbnails > img.active');
+    console.log(currentThumb);
+    // remove the active class from the active thumb
+    currentThumb.classList.remove('active');
 
-
-
-
-        // select the active thumb
-        const currentThumb = document.querySelector('.thumbnails > img.active')
-        console.log(currentThumb);
-        // remove the active class from the active thumb
-        currentThumb.classList.remove('active')
-
-    
-
-
-
-
-    
     if (activeSlide === 0) {
         activeSlide = slidesImages.length - 1
         // activeSlide = 5
@@ -237,35 +159,26 @@ slides.forEach((element,index) => {
         // decrement the activeSlide of 1
         activeSlide--
     }
-    
-    
-    console.log(activeSlide);
-    
-  
+
     // select the next slide
     const nextSlide = slidesImages[activeSlide]
     console.log(nextSlide);
     // add the active class to the next slide
     nextSlide.classList.add('active')
 
+    // select the next thumb
+    const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
+    console.log(nextThumb);
+    // add to the next thumb the active class
+    nextThumb.classList.add('active')
+
+}
 
 
 
 
-    
-
-
-
-        // select the next thumb
-        const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
-        console.log(nextThumb);
-        // add to the next thumb the active class
-        nextThumb.classList.add('active')
-
-  }
-
-  pausePlayEl.addEventListener('click', ()=> {
+pausePlayEl.addEventListener('click', () => {
     clearInterval(autoNextPlay);
     clearInterval(autoPrevPlay);
 
-  })
+})
